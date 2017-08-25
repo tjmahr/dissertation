d4d1af35b8b0d4560690be34abc7a846c5f33540
TJ Mahr <mahr@wisc.edu>
2017-08-25 08:35:27 -0500

    add tool to install github packages

---
+++ R/setup_helpers.R
@@ -0,0 +1,5 @@
+install_github_packages <- function() {
+  devtools::install_github("LearningToTalk/L2TDatabase")
+  devtools::install_github("tjmahr/littlelisteners")
+  devtools::install_github("tjmahr/lookr")
+}
