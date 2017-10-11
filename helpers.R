fed436f9c5e0060a018a21b46778f367897f43d9
TJ Mahr <mahr@wisc.edu>
2017-10-11 08:05:23 -0500

    comparing pairs

--- helpers.R
+++ helpers.R
@@ -23,6 +23,36 @@ sample_n_of <- function(data, size, ...) {
   data[subset, ]
 }
 
+create_pairs <- function(xs) {
+  if (!is.factor(xs)) xs <- ordered(xs)
+  xs %>%
+    levels() %>%
+    rev() %>%
+    combn(2) %>%
+    t() %>%
+    as.data.frame() %>%
+    set_names("x1", "x2") %>%
+    mutate(name = paste0(.data$x1, "-", .data$x2)) %>%
+    mutate_all(as.character) %>%
+    arrange(x1, desc(x2))
+}
+
+compare_pairs <- function(data, levels, values, f = `-`) {
+  levels <- enquo(levels)
+  values <- enquo(values)
+
+  pairs <- data %>% pull(!! levels) %>% create_pairs()
+  wide <- tidyr::spread(data, !! levels, !! values)
+  for (row_i in seq_len(nrow(pairs))) {
+    pair_i <- pairs[row_i, ]
+    wide[, pair_i$name] <- f(wide[[pair_i$x1]], wide[[pair_i$x2]])
+  }
+
+  wide %>%
+    select(-one_of(c(pairs$x1), c(pairs$x2))) %>%
+    tidyr::gather("pair", "value", one_of(c(pairs$name))) %>%
+    mutate(pair = factor(.data$pair, levels = pairs$name))
+}
 
 # ---- other helpers ----
 
