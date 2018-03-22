d90d0b29e3e6790a4c2fb97ae67e8aa39e193f6d
Tristan Jay Mahr <Mahr@Wisc.Edu>
2018-03-22 15:16:07 -0500

    move material to appendix

--- helpers.R
+++ helpers.R
@@ -6,3 +6,13 @@ library(rlang)
 library(littlelisteners)
 library(ggplot2)
 library(tjmisc)
+
+constants <- list(
+  x_time = "Time after target onset [ms]",
+  y_logodds_target = "Log-odds looking to target",
+  y_prop_target = "Proportion looks to target",
+  col_blue_highlight = "#0074D9",
+  col_off_black = "#111111",
+  cap_median_90_50 = "Posterior median with 90% and 50% intervals",
+  cap_90_50 = "90% and 50% intervals"
+)
