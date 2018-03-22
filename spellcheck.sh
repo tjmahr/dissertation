81a2b49433a28984db61f4322a70fa42ab013957
Tristan Jay Mahr <Mahr@Wisc.Edu>
2018-03-22 11:22:13 -0500

    add more spell checking

--- spellcheck.sh
+++ spellcheck.sh
@@ -14,6 +14,13 @@ mdspell -r -n --en-us README.md
 mdspell -r -n --en-us index.Rmd
 mdspell -r -n --en-us 01-prospectus.Rmd
 mdspell -r -n --en-us 02-specific-aims.Rmd
+mdspell -r -n --en-us 03-significance.Rmd
+mdspell -r -n --en-us 04-hypotheses.Rmd
+mdspell -r -n --en-us 05-methods.Rmd
+mdspell -r -n --en-us 06-outcomes.Rmd
+mdspell -r -n --en-us 07-analysis.Rmd
 mdspell -r -n --en-us 10-aim1-part.Rmd
 mdspell -r -n --en-us 11-aim1-notebook.Rmd
 mdspell -r -n --en-us 12-aim1-notebook.Rmd
+mdspell -r -n --en-us 15-aim1-notebook.Rmd
+mdspell -r -n --en-us 90-appendix.Rmd
