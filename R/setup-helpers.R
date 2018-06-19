db258b5f8c44fa755d5668ee4d34d6a82b14b4f4
TJ Mahr <mahr@wisc.edu>
2018-06-19 08:47:35 -0500

    work on plots for mp models

--- R/setup-helpers.R
+++ R/setup-helpers.R
@@ -10,6 +10,7 @@ install_github_packages <- function() {
   devtools::install_github("tjmahr/tristan")
   devtools::install_github("tjmahr/tjmisc")
   devtools::install_github("tjmahr/printy")
+  devtools::install_github("thomasp85/patchwork")
   devtools::install_github("klutometis/roxygen")
   devtools::install_github("rstudio/bookdown")
   devtools::install_github("crsh/papaja")
