46dff3ccf0bab69d0fe56df9c463f2098b97de8f
Tristan Jay Mahr <Mahr@Wisc.Edu>
2018-07-14 15:58:31 -0500

    get more info from the mp models

--- R/aim2-mp-model-helpers.R
+++ R/aim2-mp-model-helpers.R
@@ -3,26 +3,35 @@ library(dplyr)
 setwd(rprojroot::find_rstudio_root_file())
 
 source("./helpers.R")
+n_peaks <- 1000
 
-roll_one_peak <- function(df) {
-  df %>%
-    group_by(.draw) %>%
-    mutate(
-      med = zoo::rollmedian(.posterior_value, 5, fill = "extend")) %>%
-    top_n(1, med) %>%
-    top_n(-1, Time) %>%
-    ungroup()
+tidy_gcas <- function(data) {
+  qtidy <- purrr::quietly(broom::tidy)
+  model1 <- suppressWarnings(lm(plogis(.posterior_value) ~ ot1 + ot2 + ot3, data))
+  model2 <- suppressWarnings(lm(.posterior_value ~ ot1 + ot2 + ot3, data))
+
+  df1 <- qtidy(model1) %>%
+    getElement("result") %>%
+    select(term, estimate) %>%
+    tidyr::spread(term, estimate) %>%
+    rename(
+      Prop_Intercept = `(Intercept)`,
+      Prop_ot1 = ot1,
+      Prop_ot2 = ot2,
+      Prop_ot3 = ot3)
+
+  df2 <- qtidy(model2) %>%
+    getElement("result") %>%
+    select(term, estimate) %>%
+    tidyr::spread(term, estimate) %>%
+    rename(
+      Logit_Intercept = `(Intercept)`,
+      Logit_ot1 = ot1,
+      Logit_ot2 = ot2,
+      Logit_ot3 = ot3)
+  bind_cols(df1, df2)
 }
 
-roll_one_valley <- function(df) {
-  df %>%
-    group_by(.draw) %>%
-    mutate(
-      med = zoo::rollmedian(.posterior_value, 5, fill = "extend")) %>%
-    top_n(-1, med) %>%
-    top_n(-1, Time) %>%
-    ungroup()
-}
 
 mp_unfam <- readr::read_rds("./data/aim2-mp-unfam.rds.gz")
 d_u <- readr::read_csv("./data/aim2-mp-unfam-modeled-data.csv.gz")
@@ -35,24 +44,83 @@ d_u_to_fit <- d_u_fitted %>%
   group_by(Study, ResearchID) %>%
   split(group_indices(.))
 
-n_peaks <- 1000
-
 for (i in seq_along((d_u_to_fit))) {
   d_u_to_fit[[i]] %>%
     distinct(Study, ResearchID) %>%
     glue::glue_data("Group {i}/{length(d_u_to_fit)} ({Study} {ResearchID})") %>%
     message()
 
-  d_u_to_fit[[i]] <- augment_linpred(
-    mp_unfam, d_u_to_fit[[i]], allow_new_levels = TRUE,
-    re_formula = NULL, nsamples = n_peaks) %>%
-    select(-ot1, -ot2, -ot3, -Target, -Trials, -.observation) %>%
-    roll_one_peak() %>%
-    select(-.posterior_value)
+  rolled <- mp_unfam %>%
+    augment_linpred(
+      d_u_to_fit[[i]],
+      allow_new_levels = TRUE,
+      re_formula = NULL,
+      nsamples = n_peaks) %>%
+    select(-Target, -Trials, -.observation) %>%
+    group_by(.draw) %>%
+    mutate(
+      med = zoo::rollmedian(.posterior_value, 5, fill = "extend")) %>%
+    ungroup()
+
+    minimum <- rolled %>%
+      group_by(.draw) %>%
+      top_n(-1, med) %>%
+      top_n(-1, Time) %>%
+      ungroup() %>%
+      mutate(Valley_Prop = plogis(med)) %>%
+      rename(Valley_Time = Time, Valley_Logit = med) %>%
+      select(.draw, Study, ResearchID, starts_with("Valley"))
+
+    peak <- rolled %>%
+      group_by(.draw) %>%
+      top_n(1, med) %>%
+      top_n(-1, Time) %>%
+      ungroup() %>%
+      mutate(Peak_Prop = plogis(med)) %>%
+      rename(Peak_Time = Time, Peak_Logit = med) %>%
+      select(.draw, Study, ResearchID, starts_with("Peak"))
+
+    broomed <- rolled %>%
+      tidyr::nest(-.draw) %>%
+      mutate(
+        Prop = data %>% purrr::map(tidy_gcas)) %>%
+      select(-data) %>%
+      tidyr::unnest(Prop)
+
+    rise_after_min <- rolled %>%
+      left_join(minimum,  by = c(".draw", "Study", "ResearchID")) %>%
+      group_by(.draw) %>%
+      filter(Valley_Time <= Time) %>%
+      top_n(1, med) %>%
+      top_n(-1, Time) %>%
+      ungroup() %>%
+      mutate(
+        PeakAfterValley_Logit = med,
+        PeakAfterValley_Prop = plogis(med),
+        RiseAfterValley_Logit = PeakAfterValley_Logit - Valley_Logit,
+        RiseAfterValley_Prop = PeakAfterValley_Prop - Valley_Prop) %>%
+      select(
+        .draw, Study, ResearchID, starts_with("Valley"),
+        starts_with("PeakAfter"), starts_with("RiseAfter"))
+
+    combined <- rise_after_min %>%
+      left_join(peak, by = c(".draw", "Study", "ResearchID")) %>%
+      left_join(broomed, by = ".draw")
+
+    d_u_to_fit[[i]] <- combined
 }
 
 bind_rows(d_u_to_fit) %>%
-  readr::write_csv("aim2-mp-unfam-peaks.csv.gz")
+  readr::write_csv("./data/aim2-mp-unfam-peaks.csv.gz")
+
+rm(d_u_to_fit)
+rm(d_u)
+rm(d_u_fitted)
+rm(mp_unfam)
+
+
+
+
 
 
 d_f <- readr::read_csv("./data/aim2-mp-fam-modeled-data.csv.gz")
@@ -72,14 +140,66 @@ for (i in seq_along((d_f_to_fit))) {
     glue::glue_data("Group {i}/{length(d_f_to_fit)} ({Study} {ResearchID})") %>%
     message()
 
-  d_f_to_fit[[i]] <- augment_linpred(
-    mp_fam, d_f_to_fit[[i]], allow_new_levels = TRUE,
-    re_formula = NULL, nsamples = n_peaks) %>%
-    select(-ot1, -ot2, -ot3, -Target, -Trials, -.observation) %>%
-    roll_one_valley() %>%
-    select(-.posterior_value)
+  rolled <- mp_fam %>%
+    augment_linpred(
+      d_f_to_fit[[i]],
+      allow_new_levels = TRUE,
+      re_formula = NULL,
+      nsamples = n_peaks) %>%
+    select(-Target, -Trials, -.observation) %>%
+    group_by(.draw) %>%
+    mutate(
+      med = zoo::rollmedian(.posterior_value, 5, fill = "extend")) %>%
+    ungroup()
+
+  minimum <- rolled %>%
+    group_by(.draw) %>%
+    top_n(-1, med) %>%
+    top_n(-1, Time) %>%
+    ungroup() %>%
+    mutate(Valley_Prop = plogis(med)) %>%
+    rename(Valley_Time = Time, Valley_Logit = med) %>%
+    select(.draw, Study, ResearchID, starts_with("Valley"))
+
+  peak <- rolled %>%
+    group_by(.draw) %>%
+    top_n(1, med) %>%
+    top_n(-1, Time) %>%
+    ungroup() %>%
+    mutate(Peak_Prop = plogis(med)) %>%
+    rename(Peak_Time = Time, Peak_Logit = med) %>%
+    select(.draw, Study, ResearchID, starts_with("Peak"))
+
+  broomed <- rolled %>%
+    tidyr::nest(-.draw) %>%
+    mutate(
+      Prop = data %>% purrr::map(tidy_gcas)) %>%
+    select(-data) %>%
+    tidyr::unnest(Prop)
+
+  rise_after_min <- rolled %>%
+    left_join(minimum,  by = c(".draw", "Study", "ResearchID")) %>%
+    group_by(.draw) %>%
+    filter(Valley_Time <= Time) %>%
+    top_n(1, med) %>%
+    top_n(-1, Time) %>%
+    ungroup() %>%
+    mutate(
+      PeakAfterValley_Logit = med,
+      PeakAfterValley_Prop = plogis(med),
+      RiseAfterValley_Logit = PeakAfterValley_Logit - Valley_Logit,
+      RiseAfterValley_Prop = PeakAfterValley_Prop - Valley_Prop) %>%
+    select(
+      .draw, Study, ResearchID, starts_with("Valley"),
+      starts_with("PeakAfter"), starts_with("RiseAfter"))
+
+  combined <- rise_after_min %>%
+    left_join(peak, by = c(".draw", "Study", "ResearchID")) %>%
+    left_join(broomed, by = ".draw")
+
+  d_f_to_fit[[i]] <- combined
 }
 
 bind_rows(d_f_to_fit) %>%
-  readr::write_csv("aim2-mp-fam-peaks.csv.gz")
+  readr::write_csv("./data/aim2-mp-fam-peaks.csv.gz")
 
